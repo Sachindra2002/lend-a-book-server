9d970d3ac95c019ee9c0324c626e74a4b99c3bc6
Sachindra2002 <52739523+Sachindra2002@users.noreply.github.com>
2021-07-22 00:31:01 +0530

    added validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,23 +3,36 @@ const { sequelize, User } = require("../models");
 
 /* Register User */
 exports.signup = async (request, response) => {
+
+  var today = new Date();
+
+  const email = request.body.email;
+  const firstName = request.body.firstName;
+  const lastName = request.body.lastName;
+  const dob = request.body.dob;
+  const phoneNumber = request.body.phonenumber;
+  const password = request.body.password;
+  const year = new Date(dob);
+  const age = today.getFullYear() - year.getFullYear();
+
   const new_user = {
-    email: request.body.email,
-    firstName: request.body.firstName,
-    lastName: request.body.lastName,
-    dob: request.body.dob,
-    age: request.body.age,
-    phoneNumber: request.body.phoneNumber,
+    email,
+    firstName,
+    lastName,
+    dob,
+    age,
+    phoneNumber,
+    password,
     image: "null",
     isVerified: 0,
     isBanned: 0,
-    password: request.body.password,
     userRole: "member",
   };
 
   try {
     const user = await User.create(new_user);
     return response.json(user);
+    console.log(age);
   } catch (err) {
     console.log(err);
     return response.status(500).json(err);
