7dd153ef7a032518ffea516a0e86cd18cf1ec6d1
Sachindra2002 <52739523+Sachindra2002@users.noreply.github.com>
2021-08-02 23:48:31 +0530

    added password hashing and validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,35 @@
 const { request, response } = require("express");
 const { sequelize, User } = require("../models");
+const bcrypt = require("bcrypt");
+const { validateRegister } = require("../utils/validators");
 
 /* Register User */
 exports.signup = async (request, response) => {
-
   var today = new Date();
+
+  const todayDate = new Date();
+  var dd = String(todayDate.getDate()).padStart(2, "0");
+  var mm = String(todayDate.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = todayDate.getFullYear();
+  todayDate = yyyy + "/" + mm + "/" + dd;
+
+  const oneYearFromNow = new Date();
+  oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
 
   const email = request.body.email;
   const firstName = request.body.firstName;
   const lastName = request.body.lastName;
   const dob = request.body.dob;
-  const phoneNumber = request.body.phonenumber;
+  const phoneNumber = request.body.phoneNumber;
   const password = request.body.password;
+  const confirmPassword = request.body.confirmPassword;
   const year = new Date(dob);
   const age = today.getFullYear() - year.getFullYear();
+  const creditCardNumber = request.body.creditCardNumber;
+  const creditCardExpiryDate = request.body.creditCardExpiryDate;
+  const creditCardCvv = request.body.creditCardCvv;
+  const option = request.body.option;
+  const totalAmount = request.body.totalAmount;
 
   const new_user = {
     email,
@@ -23,6 +39,7 @@ exports.signup = async (request, response) => {
     age,
     phoneNumber,
     password,
+    confirmPassword,
     image: "null",
     isVerified: 0,
     isBanned: 0,
@@ -30,12 +47,22 @@ exports.signup = async (request, response) => {
   };
 
   try {
+    let errors = await validateRegister(new_user);
+
+    if (Object.keys(errors).length > 0)
+      return response.status(400).json({ error: errors });
+
+    //Hash password
+    new_user.password = new_user.password
+      ? await bcrypt.hash(new_user.password, 6)
+      : null;
+
+    //create new user object in database
     const user = await User.create(new_user);
+
     return response.json(user);
-    console.log(age);
   } catch (err) {
     console.log(err);
     return response.status(500).json(err);
   }
 };
-
---
+++ utils/validators.js
@@ -0,0 +1,18 @@
+//const User = require("../models/users");
+const { sequelize, User } = require("../models");
+
+exports.validateRegister = async (data) => {
+  let errors = {};
+
+  try {
+    let user = await User.findOne({ where: { email: data.email } });
+    if (user) errors.email = "Email already exists";
+
+    if (data.password !== data.confirmPassword)
+      errors.confirmPassword = "Passwords don't match";
+
+    return errors;
+  } catch (error) {
+    return error;
+  }
+};
